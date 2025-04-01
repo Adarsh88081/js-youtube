fb2736536120e4891da98e106ecd4d708a333665
Adarsh88081 <adarsh209601@gmail.com>
2025-04-01 16:27:42 +0000

    lecture number 6

---
+++ .devcontainer/basics/03_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version 
+
+//alert(3+3) we are using nodejs, not browser
+
+let name=" Adarsh" // string
+let age=19 // integer
+let isLoggedIn = false
+
+/*
+PRIMITIVE DATA TYPE
+1.number=> 2 to power 53
+2.bigint
+3.string=>" " or ''
+4.boolean => true or false
+5.null=> standalone value
+6.undefined =>
+7.symbol =>unique
+ */
+
+
+
+
+/*
+NON PRIMITIVE DATA TYPES
+1.Objects
+     null is type of oject
+*/
+console.log(typeof"adarsh");
---
+++ .devcontainer/basics/04_dataconversion.js
@@ -0,0 +1,25 @@
+let score="33"
+ 
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score) // changing type of score from string to number
+console.log(typeof valueInNumber);
+
+
+// now second example
+let score_1="33abc"
+console.log(typeof score_1);
+
+let value= Number(score_1)
+console.log(value) // if we will simply type "typeof " it will show us that it is a number instead it is not so we are doing this in order to check it
+// after running this line "NaN" will appear which means not a number
+/*
+"33"=>33
+"123as"=> NaN
+*/
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// if we will convert a empty string then it is false else everything is true
