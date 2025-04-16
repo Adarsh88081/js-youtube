a31d5db4c927baec5a12c016992024fcb4ed725f
Adarsh88081 <adarsh209601@gmail.com>
2025-04-16 10:14:10 +0000

    completed

---
+++ .devcontainer/basics/01data_typebasics.js
@@ -0,0 +1,16 @@
+"use strict";// treat all JS code as newer version
+alert("Hello World!"); // show the message in a dialog box
+// This is a single line comment
+let name ="ADARSH"; //string
+let age = 20; //number
+let isStudent = true; //boolean
+//number=>2 to power 53-1
+//null=>no value
+//undefined=>no value assigned
+// symbol=>unique value
+//bigint=>large number
+//object=>collection of key value pairs
+console.log(typepf(name)); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
